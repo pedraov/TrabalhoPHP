91834c64f4996da1eacaa59f8e23af59a528a88f
Gabriele Dominico <121459497+GabDomi@users.noreply.github.com>
2024-05-28 19:41:42 -0300

    Update recuperar_senha.php

--- recuperar_senha.php
+++ recuperar_senha.php
@@ -24,6 +24,7 @@ if (isset($_POST['email'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recuperar Senha</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h2>Recuperar Senha</h2>
