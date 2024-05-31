54e0ec9ccb312367b8493b7fcb91ccf0abaeeb85
Gabriele Dominico <121459497+GabDomi@users.noreply.github.com>
2024-05-31 18:18:05 -0300

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,17 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h2>Login Admin</h2>
+    <header class ="header">
+    <a href="#" class = "logo">iHomeCwb</a>
+
+    <nav class="navbar">
+        <a href="index.php">Home</a>
+        <a href="Sobre.php">Sobre</a>
+        <a href="Contato.php">Contato</a>
+        <a href="Login.php">Login</a>
+    </nav>
+</header>
+<h2 style="margin-top: 150px">Login Admin</h2>
     <form action="login.php" method="post" class="form">
 
       <div class="inputForm">
