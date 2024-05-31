7fb98bbaf176ed1885062a8f5a2662f53d1caf0e
Gabriele Dominico <121459497+GabDomi@users.noreply.github.com>
2024-05-31 17:13:38 -0300

    Update index.php
    Atualização das imagens

--- index.php
+++ index.php
@@ -20,25 +20,25 @@
 </header>
 
     <main>
-        <h1>Bem-vindo à nossa Loja de Celulares</h1>
-        <p>Confira nossas ofertas incríveis de smartphones.</p>
+        <h1 style="margin-top: 15px">Bem-vindo à nossa Loja de Celulares</h1>
+        <p style="margin-bottom: 10px>Confira nossas ofertas incríveis de smartphones.</p>
         <div class="produtos">
             <div class="produto">
-                <h2>Celular 1</h2>
-                <p><img src="imagens/1.jpg" alt="Celular 1"></p>
-                <p>Descrição do Celular 1.</p>
+                <h2>Iphone 15</h2>
+                <p><img src="imagens/iphone15AZUL.jpeg" alt="Iphone 15 azul claro"></p>
+                <p> 128GB | Azul Claro</p>
                 <button onclick="comprar()">Comprar</button>
             </div>
             <div class="produto">
-                <h2>Celular 2</h2>
-                <p><img src="imagens/2.jpg" alt="Celular 2"></p>
-                <p>Descrição do Celular 2.</p>
+                <h2>Iphone 14 PRO</h2>
+                <p><img src="imagens/iphone14proROXO.jpeg" alt="Iphone 14 Pro roxo"></p>
+                <p> 128GB | Roxo Midnight</p>
                 <button onclick="comprar()">Comprar</button>
             </div>
             <div class="produto">
-                <h2>Celular 3</h2>
-                <p><img src="imagens/3.jpg" alt="Celular 3"></p>
-                <p>Descrição do Celular 3.</p>
+                <h2>Iphone 13</h2>
+                <p><img src="imagens/iphone13RED.jpeg" alt="Iphone 13 vermelho"></p>
+                <p> 256GB | Vermelho</p>
                 <button onclick="comprar()">Comprar</button>
             </div>
         </div>
